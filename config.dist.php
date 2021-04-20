e0656419f5766dbb7e04b8a30414662078e02599
Mistralys <s.mordziol@gmail.com>
2021-04-20 22:38:19 +0200

    Configuration template.

---
+++ config.dist.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Absolute path to the folder in which the savegames are stored.
+ */
+const X4_SAVES_FOLDER = 'C:\Users\Argh\Documents\Egosoft\X4\70814229\save';
+
+/**
+ * Connection details for the optional server.
+ */
+const X4_SERVER_HOST = 'localhost';
+const X4_SERVER_PORT = 9494;
