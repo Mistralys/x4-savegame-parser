dcd0d5c72ec92e51f09e7b08452da0ab2fc1c218
Mistralys <s.mordziol@gmail.com>
2021-04-20 22:39:35 +0200

    Server dispatcher.

---
+++ server.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Server script: starts the savegame management server
+ * in the background.
+ */
+
+declare(strict_types=1);
+
+use Mistralys\X4Saves\Monitor\X4Server;
+
+require_once 'vendor/autoload.php';
+require_once 'config.php';
+
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+$server = new X4Server();
+$server->start();
