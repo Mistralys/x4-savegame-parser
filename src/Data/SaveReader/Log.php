9dd796c14aa2af1c495b1967d3ae4f03cc34ffa9
Mistralys <s.mordziol@gmail.com>
2021-07-18 19:56:24 +0200

    Added automatic splitting into categorized log files.

--- src/Data/SaveReader/Log.php
+++ src/Data/SaveReader/Log.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Mistralys\X4Saves\Data\SaveReader;
 
+use AppUtils\Microtime;
 use Mistralys\X4Saves\Data\SaveReader\Log\Destroyed;
 use Mistralys\X4Saves\Data\SaveReader\Log\LogEntry;
 use Mistralys\X4Saves\SaveParser\Tags\Tag\LogTag;
@@ -11,13 +12,13 @@ use Mistralys\X4Saves\SaveParser\Tags\Tag\LogTag;
 class Log extends Info
 {
     /**
-     * @var LogEntry[]
+     * @var array<string,LogEntry[]>
      */
     private array $entries = array();
 
     protected function getAutoDataName(): string
     {
-        return LogTag::SAVE_NAME;
+        return '';
     }
 
     public function getDestroyed() : Destroyed
@@ -25,39 +26,69 @@ class Log extends Info
         return new Destroyed($this);
     }
 
-    public function getEntries() : array
-    {
-        return $this->entries;
-    }
-
     public function getByCategory(string $category) : array
     {
-        $result = array();
+        $this->unpack();
 
-        foreach($this->entries as $entry)
+        if(!$this->reader->dataExists('log/'.$category)) {
+            return array();
+        }
+
+        $result = array();
+        $data = $this->reader->getRawData('log/'.$category);
+
+        foreach($data as $entryData)
         {
-            if($entry->getCategory() === $category) {
-                $result[] = $entry;
-            }
+            $result[] = new LogEntry($entryData);
         }
 
         return $result;
     }
 
-    private function load() : void
+    private function unpack() : void
+    {
+        if($this->reader->dataExists('log-unpacked')) {
+            return;
+        }
+
+        $this->reader->saveData('log-unpacked', array(
+            'date' => (new Microtime())->getMySQLDate()
+        ));
+
+        $this->autoLoad(LogTag::SAVE_NAME);
+        $this->createEntries();
+        $this->writeDataFiles();
+    }
+
+    private function writeDataFiles() : void
+    {
+        foreach ($this->entries as $categoryID => $entries)
+        {
+            $this->reader->saveData('log/'.$categoryID, $entries);
+        }
+    }
+
+    private function createEntries() : void
     {
         if(!empty($this->entries))
         {
             return;
         }
 
-        foreach ($this->data as $entry)
+        foreach ($this->data as $entryData)
         {
-            $entry = new LogEntry($entry);
+            $entry = new LogEntry($entryData);
+            $categoryID = $entry->getCategory();
 
-            if($entry->getCategory() !== LogEntry::CATEGORY_IGNORE) {
-                $this->entries[] = $entry;
+            if($categoryID === LogEntry::CATEGORY_IGNORE) {
+                continue;
             }
+
+            if(!isset($this->entries[$categoryID])) {
+                $this->entries[$categoryID] = array();
+            }
+
+            $this->entries[$categoryID][] = $entryData;
         }
     }
 }
