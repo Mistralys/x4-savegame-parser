be57104916776629e4253bf2f832d2c8142bcd2e
Mistralys <s.mordziol@gmail.com>
2023-05-04 14:45:54 +0200

    Added logging methods.

--- tests/classes/X4ParserTestCase.php
+++ tests/classes/X4ParserTestCase.php
@@ -4,12 +4,15 @@ declare(strict_types=1);
 
 namespace X4\SaveGameParserTests\TestClasses;
 
+use AppUtils\ClassHelper;
 use PHPUnit\Framework\TestCase;
 
 abstract class X4ParserTestCase extends TestCase
 {
     protected string $filesFolder;
     protected string $saveGameFile;
+    private bool $logging = false;
+    private ?string $logPrefix = null;
 
     protected function setUp() : void
     {
@@ -17,5 +20,46 @@ abstract class X4ParserTestCase extends TestCase
 
         $this->filesFolder = __DIR__.'/../files';
         $this->saveGameFile = __DIR__.'/../files/quicksave.xml';
+
+        $this->disableLogging();
+    }
+
+    public function enableLogging() : void
+    {
+        $this->logging = true;
+    }
+
+    public function disableLogging() : void
+    {
+        $this->logging = false;
+    }
+
+    public function isLoggingEnabled() : bool
+    {
+        return $this->logging;
+    }
+
+    protected function log(string $message, ...$params) : void
+    {
+        if($this->logging === false) {
+            return;
+        }
+
+        if(!isset($this->logPrefix)) {
+            $this->logPrefix = sprintf(
+                'Test [%s] | ',
+                ClassHelper::getClassTypeName($this)
+            );
+        }
+
+        if(empty($params)) {
+            echo $this->logPrefix.$message.PHP_EOL;
+            return;
+        }
+
+        echo sprintf(
+            $this->logPrefix.$message.PHP_EOL,
+            ...$params
+        );
     }
 }
