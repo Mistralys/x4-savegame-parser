bd73ee5434053a0943ee1f9a9019014c11b44d77
Mistralys <s.mordziol@gmail.com>
2023-05-05 11:23:54 +0200

    Updated to use the `SaveSelector`.

--- tests/testsuites/Parser/SplitFileTests.php
+++ tests/testsuites/Parser/SplitFileTests.php
@@ -6,6 +6,7 @@ namespace X4\SaveGameParserTests\Tests;
 
 use AppUtils\FileHelper;
 use AppUtils\FileHelper\FileInfo;
+use Mistralys\X4\SaveViewer\Parser\SaveSelector;
 use Mistralys\X4\SaveViewer\SaveParser;
 use X4\SaveGameParserTests\TestClasses\X4ParserTestCase;
 
@@ -33,64 +34,23 @@ final class SplitFileTests extends X4ParserTestCase
     {
         $this->enableLogging();
 
-        $files = FileHelper::createFileFinder('J:\OneDrive\Documents\Egosoft\X4\70814229\save')
-            ->includeExtensions(array('gz', 'xml'))
-            ->setPathmodeAbsolute()
-            ->getAll();
+        $save = SaveSelector::create('J:\OneDrive\Documents\Egosoft\X4\70814229\save')
+            ->setLoggingEnabled($this->isLoggingEnabled())
+            ->requireMostRecent();
 
-        usort($files, static function (string $a, string $b) : int {
-            return filemtime($b) - filemtime($a);
-        });
+        $this->log('Detected most recent file: [%s].', $save->getName());
 
-        $this->assertNotEmpty($files);
-
-        $mostRecent = FileInfo::factory(array_shift($files));
-
-        $this->log('Detected most recent file: [%s].', $mostRecent->getName());
-
-        $modTime = $mostRecent->getModifiedDate();
-
-        if($mostRecent->getExtension() === 'gz')
+        if(!$save->isUnzipped())
         {
-            $this->log('File is zipped, unzipping.');
-
-            $outFile = FileInfo::factory(str_replace('.xml.gz', '.xml', $mostRecent->getPath()));
-
-            $this->unzip($mostRecent, $outFile);
-
-            $mostRecent = $outFile;
+            $this->log('Unzipping the file.');
+            $save->unzip();
         }
 
-        $parser = new SaveParser($mostRecent, $this->filesFolder, $modTime);
-        $parser->setLoggingEnabled($this->isLoggingEnabled());
+        $parser = SaveParser::create($save, $this->filesFolder)
+            ->setAutoBackupEnabled()
+            ->setLoggingEnabled($this->isLoggingEnabled())
+            ->unpack();
 
-        FileHelper::deleteTree($parser->getOutputPath());
-
-        $parser->unpack();
-    }
-
-    private function unzip(FileInfo $sourceFile, FileInfo $targetFile) : void
-    {
-        $this->log('Unzipping file [%s].', $sourceFile->getName());
-
-        // Raising this value may increase performance
-        $buffer_size = 4096; // read 4kb at a time
-
-        // Open our files (in binary mode)
-        $file = gzopen($sourceFile->getPath(), 'rb');
-        $out_file = fopen($targetFile->getPath(), 'wb');
-
-        // Keep repeating until the end of the input file
-        while(!gzeof($file)) {
-            // Read buffer-size bytes
-            // Both fwrite and gzread and binary-safe
-            fwrite($out_file, gzread($file, $buffer_size));
-        }
-
-        // Files are done, close files
-        fclose($out_file);
-        gzclose($file);
-
-        $this->log('Unzip complete.');
+        $this->assertTrue($parser->hasBackup());
     }
 }
