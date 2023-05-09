40440b4e1d2bb72e7d95568a1b97af9851fa3503
Mistralys <s.mordziol@gmail.com>
2023-05-09 07:23:41 +0200

    Create MainSave.php

---
+++ src/X4/SaveViewer/Data/SaveManager/SaveTypes/MainSave.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mistralys\X4\SaveViewer\SaveManager\SaveTypes;
+
+use Mistralys\X4\SaveViewer\Data\BaseSaveFile;
+
+class MainSave extends BaseSaveFile
+{
+}
