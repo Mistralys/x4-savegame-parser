f7ac520f1ad98beab1814cc21f14e2447c55d9cc
Mistralys <s.mordziol@gmail.com>
2023-05-10 22:11:23 +0200

    Added `getReader()` and `getSave()`.

--- src/X4/SaveViewer/UI/SubPage.php
+++ src/X4/SaveViewer/UI/SubPage.php
@@ -4,8 +4,19 @@ declare(strict_types=1);
 
 namespace Mistralys\X4\SaveViewer\UI;
 
+use Mistralys\X4\SaveViewer\Data\BaseSaveFile;
+use Mistralys\X4\SaveViewer\Data\SaveReader;
 use Mistralys\X4\UI\BaseSubPage;
 
 abstract class SubPage extends BaseSubPage
 {
+    public function getReader() : SaveReader
+    {
+        return $this->page->getReader();
+    }
+
+    public function getSave() : BaseSaveFile
+    {
+        return $this->page->getSave();
+    }
 }
