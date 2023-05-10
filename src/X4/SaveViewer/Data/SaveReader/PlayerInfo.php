4765579a89ef13d931f87ae1be64611dc5dd8adb
Mistralys <s.mordziol@gmail.com>
2023-05-10 16:59:16 +0200

    Updated to use the new JSON file.

--- src/X4/SaveViewer/Data/SaveReader/PlayerInfo.php
+++ src/X4/SaveViewer/Data/SaveReader/PlayerInfo.php
@@ -4,50 +4,31 @@ declare(strict_types=1);
 
 namespace Mistralys\X4\SaveViewer\Data\SaveReader;
 
-use AppUtils\Microtime;
-use Mistralys\X4\SaveViewer\Parser\Tags\Tag\SaveInfoTag;
+use AppUtils\FileHelper\JSONFile;
+use Mistralys\X4\SaveViewer\Parser\Types\PlayerType;
 
 class PlayerInfo extends Info
 {
-    protected function getAutoDataName(): string
+    protected function init() : void
     {
-        return SaveInfoTag::SAVE_NAME;
+        $data = JSONFile::factory($this->collections
+            ->player()
+            ->getFilePath()
+        )
+            ->parse();
+
+        if(isset($data[PlayerType::TYPE_ID][0])) {
+            $this->data = $data[PlayerType::TYPE_ID][0];
+        }
     }
 
-    public function getSaveName() : string
+    public function getName() : string
     {
-        return $this->getStringKey(SaveInfoTag::KEY_SAVE_NAME);
+        return $this->getString(PlayerType::KEY_NAME);
     }
 
-    public function getSaveDate() : Microtime
+    public function getCode() : string
     {
-        $date = new Microtime();
-        $date->setTimestamp($this->getIntKey(SaveInfoTag::KEY_SAVE_DATE));
-        return $date;
-    }
-
-    public function getPlayerName() : string
-    {
-        return $this->getStringKey(SaveInfoTag::KEY_PLAYER_NAME);
-    }
-
-    public function getMoney() : int
-    {
-        return $this->getIntKey(SaveInfoTag::KEY_PLAYER_MONEY);
-    }
-
-    public function getMoneyPretty() : string
-    {
-        return number_format($this->getMoney(), 0, '.', ' ');
-    }
-
-    public function getGameGUID() : string
-    {
-        return $this->getStringKey(SaveInfoTag::KEY_GAME_GUID);
-    }
-
-    public function getGameCode() : int
-    {
-        return $this->getIntKey(SaveInfoTag::KEY_GAME_CODE);
+        return $this->getString(PlayerType::KEY_CODE);
     }
 }
