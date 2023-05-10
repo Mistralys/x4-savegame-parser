ae0bb8a1fe10a1208773d346998eb2318587a2bd
Mistralys <s.mordziol@gmail.com>
2023-05-10 16:50:01 +0200

    Added missing constants and some docs.

--- config.dist.php
+++ config.dist.php
@@ -1,12 +1,24 @@
 <?php
 
 /**
- * Absolute path to the folder in which the savegames are stored.
+ * Absolute path to the folder in which the X4 savegames are stored.
  */
 const X4_SAVES_FOLDER = 'C:\Users\Argh\Documents\Egosoft\X4\70814229\save';
 
 /**
- * Connection details for the optional server.
+ * Absolute path to where unpacked savegame data and backups are stored.
+ * Note: Will be created if it does not exist.
+ */
+const X4_STORAGE_FOLDER = __DIR__.'/archived-saves';
+
+/**
+ * Server host from which to run the UI.
+ * This is typically "localhost" or "127.0.0.1".
  */
 const X4_SERVER_HOST = 'localhost';
+
+/**
+ * Server port to use to access the UI.
+ * You can change this to any open port on your machine.
+ */
 const X4_SERVER_PORT = 9494;
