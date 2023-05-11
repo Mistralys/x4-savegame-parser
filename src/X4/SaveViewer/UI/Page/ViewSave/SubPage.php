10870eadd0063980349162613f2e23147c2cd9f8
Mistralys <s.mordziol@gmail.com>
2023-05-11 08:55:15 +0200

    Removed unneeded properties.

--- src/X4/SaveViewer/UI/Page/ViewSave/SubPage.php
+++ src/X4/SaveViewer/UI/Page/ViewSave/SubPage.php
@@ -4,19 +4,13 @@ declare(strict_types=1);
 
 namespace Mistralys\X4\SaveViewer\UI\Pages\ViewSave;
 
-use Mistralys\X4\SaveViewer\Data\BaseSaveFile;
-use Mistralys\X4\SaveViewer\Data\SaveReader;
 use Mistralys\X4\SaveViewer\UI\Pages\ViewSave;
 
 /**
  * @property ViewSave $page
- *
  */
 abstract class SubPage extends \Mistralys\X4\SaveViewer\UI\SubPage
 {
-    protected SaveReader $reader;
-    protected BaseSaveFile $save;
-
     protected function getURLParams() : array
     {
         return array();
