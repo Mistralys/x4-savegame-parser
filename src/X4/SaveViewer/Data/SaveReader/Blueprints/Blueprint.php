d2fbc059ea27b68d3e959dbe7a8599c7aa64712e
Mistralys <s.mordziol@gmail.com>
2023-05-20 23:35:22 +0200

    Added race detection.

--- src/X4/SaveViewer/Data/SaveReader/Blueprints/Blueprint.php
+++ src/X4/SaveViewer/Data/SaveReader/Blueprints/Blueprint.php
@@ -4,16 +4,51 @@ declare(strict_types=1);
 
 namespace Mistralys\X4\SaveViewer\Data\SaveReader\Blueprints;
 
+use Mistralys\X4\Database\Races\RaceDef;
+use Mistralys\X4\Database\Races\RaceDefs;
+
 class Blueprint
 {
     private string $name;
 
     private BlueprintCategory $category;
+    private ?RaceDef $race = null;
 
     public function __construct(BlueprintCategory $category, string $name)
     {
         $this->category = $category;
         $this->name = $name;
+
+        $this->detectRace();
+    }
+
+    public function getRace() : ?RaceDef
+    {
+        return $this->race;
+    }
+
+    public function getRaceID() : string
+    {
+        if(isset($this->race)) {
+            return $this->race->getID();
+        }
+
+        return RaceDefs::RACE_GENERIC;
+    }
+
+    public function detectRace() : void
+    {
+        $collection = RaceDefs::getInstance();
+        $raceIDs = $collection->getIDs();
+        $parts = explode('_', $this->getName());
+
+        foreach($raceIDs as $raceID)
+        {
+            if(in_array($raceID, $parts, true)) {
+                $this->race = $collection->getByID($raceID);
+                return;
+            }
+        }
     }
 
     /**
