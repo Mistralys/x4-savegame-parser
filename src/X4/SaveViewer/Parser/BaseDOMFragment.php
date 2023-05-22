6fa6ce7b72269eb21ee61f3c1b774c0794be321b
Mistralys <s.mordziol@gmail.com>
2023-05-22 07:47:59 +0200

    Added `parseElementAttributes()`.

--- src/X4/SaveViewer/Parser/BaseDOMFragment.php
+++ src/X4/SaveViewer/Parser/BaseDOMFragment.php
@@ -25,4 +25,19 @@ abstract class BaseDOMFragment extends BaseFragment
 
         $this->parseDOM($dom);
     }
+
+    protected function parseElementAttributes(DOMElement $element) : array
+    {
+        if(empty($element->attributes)) {
+            return array();
+        }
+
+        $result = array();
+        foreach($element->attributes as $attributeNode)
+        {
+            $result[$attributeNode->nodeName] = $attributeNode->nodeValue;
+        }
+
+        return $result;
+    }
 }
