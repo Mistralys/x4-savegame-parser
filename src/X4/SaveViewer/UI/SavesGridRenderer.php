794346d44d2e5cba3d3d4c6c0cc1c98b7f94b93c
Mistralys <s.mordziol@gmail.com>
2023-05-23 18:48:38 +0200

    Added losses column and delete button.

--- src/X4/SaveViewer/UI/SavesGridRenderer.php
+++ src/X4/SaveViewer/UI/SavesGridRenderer.php
@@ -6,6 +6,7 @@ namespace Mistralys\X4\SaveViewer\UI;
 
 use AppUtils\Interfaces\RenderableInterface;
 use AppUtils\Traits\RenderableTrait;
+use Mistralys\X4\SaveViewer\Data\ArchivedSave;
 use Mistralys\X4\SaveViewer\Data\BaseSaveFile;
 use Mistralys\X4\UI\Button;
 use Mistralys\X4\UI\Icon;
@@ -20,6 +21,7 @@ class SavesGridRenderer implements RenderableInterface
     use RenderableTrait;
 
     public const COL_SAVE_TYPE = 'save-type';
+    public const LAST_X_HOURS = 12;
 
     /**
      * @var BaseSaveFile[]
@@ -34,6 +36,7 @@ class SavesGridRenderer implements RenderableInterface
     private GridColumn $cActions;
     private GridColumn $cType;
     private GridColumn $cKhaak;
+    private GridColumn $cLosses;
 
     /**
      * @var array<string,bool>
@@ -43,9 +46,11 @@ class SavesGridRenderer implements RenderableInterface
     public function __construct(UserInterface $ui, array $saves)
     {
         $this->saves = $saves;
-
         $this->grid = $ui->createDataGrid();
+    }
 
+    private function configureGrid() : void
+    {
         $this->cName = $this->grid->addColumn('name', t('Name'));
 
         if($this->isColEnabled(self::COL_SAVE_TYPE))
@@ -56,7 +61,10 @@ class SavesGridRenderer implements RenderableInterface
         $this->cChar = $this->grid->addColumn('character', t('Character'));
         $this->cMoney = $this->grid->addColumn('money', t('Money'))
             ->alignRight();
-        $this->cKhaak = $this->grid->addColumn('khaak', t('Khaak'));
+        $this->cLosses = $this->grid->addColumn('losses', t('Losses %1$sh', '&lt;'.self::LAST_X_HOURS))
+            ->alignRight();
+        $this->cKhaak = $this->grid->addColumn('khaak', t('Khaak'))
+            ->alignRight();
         $this->cModified = $this->grid->addColumn('modified', t('Modified'));
         $this->cBackup = $this->grid->addColumn('backup', t('Backup?'))
             ->alignCenter();
@@ -77,6 +85,8 @@ class SavesGridRenderer implements RenderableInterface
 
     public function render() : string
     {
+        $this->configureGrid();
+
         foreach($this->saves as $save)
         {
             $row = $this->grid->createRow();
@@ -88,11 +98,25 @@ class SavesGridRenderer implements RenderableInterface
                 $saveInfo = $reader->getSaveInfo();
                 $date = $saveInfo->getSaveDate();
                 $khaak = $reader->getKhaakStations();
+                $losses = $reader->getShipLosses();
 
                 $row->setValue($this->cName, sb()->link($save->getSaveName(), $save->getURLView())->add('-')->add($saveInfo->getSaveName()));
                 $row->setValue($this->cChar, $saveInfo->getPlayerName());
                 $row->setValue($this->cMoney, $saveInfo->getMoneyPretty());
                 $row->setValue($this->cKhaak, sb()->link((string)count($khaak->getSectors()), $khaak->getURLView()));
+                $row->setValue($this->cLosses, sb()->link((string)$losses->countLosses(self::LAST_X_HOURS), $losses->getURLView()));
+
+                if($save instanceof ArchivedSave)
+                {
+                    $row->setValue($this->cActions,
+                        Button::create(t('Delete...'))
+                            ->setIcon(Icon::delete())
+                            ->colorDanger()
+                            ->sizeExtraSmall()
+                            ->setTooltip(t('Displays a confirmation page to delete the archived save.'))
+                            ->link($save->getURLDelete())
+                    );
+                }
             }
             else
             {
@@ -101,14 +125,16 @@ class SavesGridRenderer implements RenderableInterface
                 $row->setValue($this->cName, $save->getSaveName());
                 $row->setValue($this->cChar, '-');
                 $row->setValue($this->cMoney, '-');
+                $row->setValue($this->cLosses, '-');
 
+                /*
                 $row->setValue($this->cActions,
                     Button::create(t('Unpack'))
                         ->setIcon(Icon::unpack())
                         ->colorPrimary()
                         ->sizeExtraSmall()
                         ->link($save->getURLUnpack())
-                );
+                );*/
             }
 
             /*
