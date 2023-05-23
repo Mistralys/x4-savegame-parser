98036591bb1beb21e71ba39a2c4190d7f2f807e8
Mistralys <s.mordziol@gmail.com>
2023-05-23 18:51:46 +0200

    Added `deleteArchive()`.

--- src/X4/SaveViewer/Data/SaveManager/SaveTypes/ArchivedSave.php
+++ src/X4/SaveViewer/Data/SaveManager/SaveTypes/ArchivedSave.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Mistralys\X4\SaveViewer\Data;
 
+use AppUtils\FileHelper;
 use function AppLocalize\t;
 
 class ArchivedSave extends BaseSaveFile
@@ -11,5 +12,10 @@ class ArchivedSave extends BaseSaveFile
     public function getTypeLabel() : string
     {
         return t('Archived savegame');
+    }
+
+    public function deleteArchive() : void
+    {
+        FileHelper::deleteTree($this->getStorageFolder());
     }
 }
