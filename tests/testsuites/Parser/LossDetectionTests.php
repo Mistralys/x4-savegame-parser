0016a78f3e621aa5cef6e75b26e6f24f2ddf5a0f
Mistralys <s.mordziol@gmail.com>
2023-05-23 18:37:02 +0200

    Added ship loss tests.

---
+++ tests/testsuites/Parser/LossDetectionTests.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace X4\SaveGameParserTests\Tests;
+
+use AppUtils\FileHelper\JSONFile;
+use Mistralys\X4\SaveViewer\Parser\Collections;
+use Mistralys\X4\SaveViewer\Parser\DataProcessing\Processors\DetectShipLosses;
+use Mistralys\X4\SaveViewer\Parser\Types\LogEntryType;
+use X4\SaveGameParserTests\TestClasses\X4ParserTestCase;
+
+final class LossDetectionTests extends X4ParserTestCase
+{
+    public function test_detectLoss() : void
+    {
+        $result = DetectShipLosses::parseTitle('ST+ TEL VTJ - Boa (LUD-229) was destroyed.');
+
+        $this->assertNotNull($result);
+        $this->assertSame('LUD-229', $result['shipCode']);
+        $this->assertSame('ST+ TEL VTJ - Boa', $result['shipName']);
+    }
+
+    public function test_parseText() : void
+    {
+        $result = DetectShipLosses::parseText("Location: Windfall IV Aurora's Dream[\\012]Commander: FO TEL Hull Parts Forge II (VTJ-380)[\\012]Destroyed by: FAF Marauder Dragon Raider (MIE-184)");
+
+        $this->assertSame("Windfall IV Aurora's Dream", $result['location']);
+        $this->assertSame("FO TEL Hull Parts Forge II (VTJ-380)", $result['commander']);
+        $this->assertSame("FAF Marauder Dragon Raider (MIE-184)", $result['destroyed by']);
+    }
+
+    public function test_parseTextNoOriginator() : void
+    {
+        $result = DetectShipLosses::parseText("Location: Great Reef[\\012]Commander: FO BOR Quantum Tube Forge (WHS-258)");
+
+        $this->assertSame("Great Reef", $result['location']);
+        $this->assertEmpty($result['destroyed by']);
+    }
+
+    public function test_loadFromJSON() : void
+    {
+        $data = JSONFile::factory(__DIR__.'/../../files/log-destroyed.json')->parse();
+
+        $this->assertCount(3, $data);
+
+        $collections = new Collections(X4_STORAGE_FOLDER);
+        $entry = new LogEntryType($collections, $data[0]);
+
+        $info = DetectShipLosses::parseEntry($entry);
+
+        $this->assertNotNull($info);
+        $this->assertSame('BIE-447', $info['shipCode']);
+        $this->assertSame('Aramean Shipyard TEL (IJU-471)', $info['commander']);
+    }
+}
