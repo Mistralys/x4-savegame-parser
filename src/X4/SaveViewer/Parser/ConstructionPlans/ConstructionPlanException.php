0bfacbf591e34f484dac4727081b098323ad6f22
Mistralys <s.mordziol@gmail.com>
2023-06-06 17:47:31 +0200

    Specialized exception.

---
+++ src/X4/SaveViewer/Parser/ConstructionPlans/ConstructionPlanException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mistralys\X4\SaveViewer\Parser\ConstructionPlans;
+
+use Mistralys\X4\SaveViewer\SaveViewerException;
+
+class ConstructionPlanException extends SaveViewerException
+{
+
+}
