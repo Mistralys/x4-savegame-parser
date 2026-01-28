af16edd3cbea4a4c1375fe50b0fec42162a90255
Mistralys <s.mordziol@gmail.com>
2026-01-28 21:22:17 +0100

    Updated to use JSON config.

--- bin/php/run-monitor.php
+++ bin/php/run-monitor.php
@@ -12,11 +12,12 @@ declare(strict_types=1);
 namespace Mistralys\X4\SaveViewer\Bin;
 
 use Mistralys\X4\SaveViewer\Monitor\X4Monitor;
+use Mistralys\X4\SaveViewer\Config\Config;
 
 require_once __DIR__.'/prepend.php';
 
 runMonitor((new X4Monitor())
-    ->optionKeepXML(X4_MONITOR_KEEP_XML)
-    ->optionAutoBackup(X4_MONITOR_AUTO_BACKUP)
-    ->optionLogging(X4_MONITOR_LOGGING)
+    ->optionKeepXML(Config::getBool('X4_MONITOR_KEEP_XML', false))
+    ->optionAutoBackup(Config::getBool('X4_MONITOR_AUTO_BACKUP', true))
+    ->optionLogging(Config::getBool('X4_MONITOR_LOGGING', false))
 );
